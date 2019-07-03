660346829687aa24049186ebed76f63d1ff644a1
StormWangxhu <StormWangxhu@163.com>
2019-07-03 14:17:28 +0800

    两种线程的启动方式

---
+++ app/src/main/java/com/stormwangxhu/androidbasic/service/threaddemo/MyThreadExtendsDemo1.java
@@ -0,0 +1,11 @@
+package com.stormwangxhu.androidbasic.service.threaddemo;
+
+
+public class MyThreadExtendsDemo1 extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("Start Thread By Extends...");
+        super.run();
+    }
+}
---
+++ app/src/main/java/com/stormwangxhu/androidbasic/service/threaddemo/MyThreadImplDemo2.java
@@ -0,0 +1,8 @@
+package com.stormwangxhu.androidbasic.service.threaddemo;
+
+public class MyThreadImplDemo2 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Start Thraed By Impl ...");
+    }
+}
